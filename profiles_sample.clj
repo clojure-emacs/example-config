c82abce32a397755981e14813b7ce47e516a8684
Benedek Fazekas <benedek.fazekas@gmail.com>
2015-06-26 14:36:31 +0100

    follow up cider release 0.9.1

--- profiles_sample.clj
+++ profiles_sample.clj
@@ -1,3 +1,3 @@
-{:user {:plugins [[cider/cider-nrepl "0.8.2"]
+{:user {:plugins [[cider/cider-nrepl "0.9.1"]
                   [refactor-nrepl "1.0.5"]]
         :dependencies [[acyclic/squiggly-clojure "0.1.2-SNAPSHOT"]]}}

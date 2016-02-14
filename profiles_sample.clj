8f39e83768746e0e6500b4dbfa23d2033b1efc32
Benedek Fazekas <benedek.fazekas@gmail.com>
2016-02-14 18:35:40 +0000

    Upgrade cider and refactor middlewares
    Also use the `repl` profile instead of the `user`

--- profiles_sample.clj
+++ profiles_sample.clj
@@ -1,3 +1,4 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.1"]
-                  [refactor-nrepl "1.1.0"]]
-        :dependencies [[acyclic/squiggly-clojure "0.1.2-SNAPSHOT"]]}}
+{:repl {:plugins [[cider/cider-nrepl "0.10.2"]
+                  [refactor-nrepl "2.0.0"]]
+        :dependencies [[acyclic/squiggly-clojure "0.1.2-SNAPSHOT"]
+                       [org.clojure/tools.nrepl "0.2.12"]]}}
